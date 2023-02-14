d9c7b1c35bb723cfe4ee6ba0b4a9bd401ee0c710
Jakob Stuber <skajake@gmail.com>
2023-02-14 13:21:46 -0600

    Make panning public

--- Sources/MultiSlider.swift
+++ Sources/MultiSlider.swift
@@ -34,7 +34,7 @@ open class MultiSlider: UIControl {
     /// generate haptic feedback when hitting snap steps
     @IBInspectable open dynamic var isHapticSnap: Bool = true
     
-    var panning = false
+    public var panning = false
 
     @IBInspectable open dynamic var thumbCount: Int {
         get {
